83ca49e8868a8bee4b12dea74e29f0db0d6dbfa9
Ujjwalb09 <ujjwalbhatt09@gmail.com>
2023-04-20 20:13:09 +0530

    Project completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // obj.name = "Ujjwal";
+
+        //name has private access cannot be set
+        // through class object
+
+        obj.setName("Ujjwal");
+
+        System.out.println(obj.getName());
+    }
 }
